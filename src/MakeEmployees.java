2180ca18729aa958f50600b04e81c9076f2c2f2f
dyla1920 <dyla1920@ISS147D8-14.scdsb.pri>
2014-10-02 08:42:28 -0400

    Initial commit

---
+++ src/MakeEmployees.java
@@ -0,0 +1,11 @@
+import TerminalIO.*;
+
+public class MakeEmployees {
+
+    public static void main(String[] args) {
+        KeyboardReader kr = new KeyboardReader(); 
+        String name = kr.readLine("Enter Name ->");
+        System.out.println("The name is "+name);
+    }
+    
+}
